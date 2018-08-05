9df8fdfa7c82a9fe4f73f08b07517ed2696a2ce2
karen S <buzz.kyt@gmail.com>
2018-08-05 20:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,15 +87,6 @@ function endGame() {
   return alert('YOU LOSE!');
 }
 
-// implement me!
-  /**
-   * This function should call `moveDodgerLeft()`
-   * if the left arrow is pressed and `moveDodgerRight()`
-   * if the right arrow is pressed. (Check the constants
-   * we've declared for you above.)
-   * And be sure to use the functions declared below!
-   */
-
 
 function moveDodger(e) {
   
